3ec4cdd945e97dbba64c2b3658f8bce634600fc3
garyjo <gary.uppiah@icloud.com>
2017-03-06 15:31:43 -0500

    Creation de l'application qui montre les villes du GTA

---
+++ g300096005/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+// import { Component,OnInit } from '@angular/core';
+// import { HomeService } from './home.service';
+
+// @Component({
+//   selector: 'home-page',
+//   templateUrl: './home.component.html',
+// })
+// export class HomeComponent implements OnInit{
+
+// public home: Array<any>;
+
+//   constructor(private homeService:HomeService){
+//     this.home = [];
+//   }
+
+//   ngOnInit(){
+//     this.home = this.homeService.homeInit();
+//     console.log("ngOnInit");
+//   }
+// }
